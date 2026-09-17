3ae852e4018cf3e2d9530754f7df32696d734edd
Qwertsyi <qwertsyik@gmail.com>
2026-09-17 21:11:27 +0300

    Register advanced wallet features

--- server/__init__.py
+++ server/__init__.py
@@ -11,7 +11,9 @@ from . import backend_app as backend_app
 from .extended_features import register_extended_features
 from .promo_notify import register_promo_notify
 from .instant_notifications import register_instant_notifications
+from .advanced_features import register_advanced_features
 
 register_extended_features(backend_app.app)
 register_promo_notify(backend_app.app)
 register_instant_notifications()
+register_advanced_features(backend_app.app)
