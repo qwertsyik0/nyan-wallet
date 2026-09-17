e01ce795f19e57017c7c277ac78ff6bcefb1cfc6
Qwertsyi <qwertsyik@gmail.com>
2026-09-17 23:07:58 +0300

    Register achievements system

--- server/__init__.py
+++ server/__init__.py
@@ -13,9 +13,11 @@ from .promo_notify import register_promo_notify
 from .instant_notifications import register_instant_notifications
 from .advanced_features import register_advanced_features
 from .release_hardening import register_release_hardening
+from .achievements import register_achievements
 
 register_extended_features(backend_app.app)
 register_promo_notify(backend_app.app)
 register_instant_notifications()
 register_advanced_features(backend_app.app)
 register_release_hardening(backend_app.app)
+register_achievements(backend_app.app)
