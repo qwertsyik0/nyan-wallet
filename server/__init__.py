5d4c29b9101be4d9f910082c94f84a54e584fbdb
Qwertsyi <qwertsyik@gmail.com>
2026-09-18 15:53:41 +0300

    Register Nyan giveaway backend

--- server/__init__.py
+++ server/__init__.py
@@ -13,11 +13,11 @@ from .promo_notify import register_promo_notify
 from .instant_notifications import register_instant_notifications
 from .advanced_features import register_advanced_features
 from .release_hardening import register_release_hardening
-from .achievements import register_achievements
+from .achievements import register_achievements\nfrom .giveaways import register_giveaways
 
 register_extended_features(backend_app.app)
 register_promo_notify(backend_app.app)
 register_instant_notifications()
 register_advanced_features(backend_app.app)
 register_release_hardening(backend_app.app)
-register_achievements(backend_app.app)
+register_achievements(backend_app.app)\nregister_giveaways(backend_app.app)
